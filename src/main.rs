c782de8bf2cacd0fc7976d0a1da191dcab66d97b
David Marchante <davidmarchan@gmail.com>
2024-02-11 23:24:59 -0500

    feat: accept single connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        let (socket, addr) = listener.accept().await.unwrap();
+
+        println!("Connection from {addr:?}");
+
+        process(socket).await;
+    }
+}
+
+async fn process(socket: TcpStream) {
+    use mini_redis::{Connection, Frame};
+
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("GOT {frame:?}");
+
+        let response = Frame::Error("unimplemented".to_string());
+        connection.write_frame(&response).await.unwrap();
+    }
 }
